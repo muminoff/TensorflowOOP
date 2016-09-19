ed1bfc0d305509bdb22fcb9939606979ef8288eb
= <=>
2016-09-18 21:43:16 -0700

    First Commit

---
+++ neuralNet.py
@@ -0,0 +1,89 @@
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+import requests
+import json
+import time
+
+class NeuralNetwork:
+
+	def __init__(self, input_layer, hidden_1, hidden_2, hidden_3, output_layer):
+		self.input_node = input_layer
+		self.n_classes = output_layer
+		self.n_nodes_hl1 = hidden_1
+		self.n_nodes_hl2 = hidden_2
+		self.n_nodes_hl3 = hidden_3
+
+		self.x = tf.placeholder('float', [None, self.input_node])
+		self.y = tf.placeholder('float', [None, self.n_classes])
+
+		self.prediction = self.neural_network_model(self.x)
+		self.cost = 0.5 * tf.reduce_sum(tf.sub(self.prediction, self.y) * tf.sub(self.prediction, self.y))
+		# self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(self.prediction, self.y))
+		# self.optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.05).minimize(self.cost)
+		# self.optimizer = tf.train.AdamOptimizer(learning_rate=0.05).minimize(self.cost)
+		self.optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(self.cost)
+
+		self.sess = tf.Session()
+		self.sess.run(tf.initialize_all_variables())
+
+	def neural_network_model(self, data):
+
+		hidden_1_layer = {
+			'weights': tf.Variable(tf.random_normal([self.input_node, self.n_nodes_hl1])),
+			'biases': tf.Variable(tf.random_normal([self.n_nodes_hl1]))
+		}
+
+		hidden_2_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_nodes_hl1, self.n_nodes_hl2])),
+			'biases': tf.Variable(tf.random_normal([self.n_nodes_hl2]))
+		}
+
+		hidden_3_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_nodes_hl2, self.n_nodes_hl3])),
+			'biases': tf.Variable(tf.random_normal([self.n_nodes_hl3]))
+		}
+
+		output_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_nodes_hl3, self.n_classes])),
+			'biases': tf.Variable(tf.random_normal([self.n_classes]))
+		}
+
+		l1 = tf.nn.softplus(tf.matmul(self.x, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+
+		l2 = tf.nn.softplus(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+
+		l3 = tf.nn.softplus(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+
+		output = tf.nn.softplus(tf.matmul(l3, output_layer['weights']) + output_layer['biases'])
+
+		return output
+
+	def train(self, input_data, output_data, epochs):
+		hm_epochs = epochs
+		for epoch in range(hm_epochs):
+			epoch_loss = 0
+			epoch_x = input_data
+			epoch_y = output_data
+			_, c = self.sess.run([self.optimizer, self.cost], feed_dict={self.x: epoch_x, self.y: epoch_y})
+			epoch_loss += c
+			print('Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss)
+
+	def predict(self, input_data):
+		p = self.sess.run(self.prediction, feed_dict={self.x: input_data})
+		return p[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ playground.py
@@ -0,0 +1,38 @@
+from neuralNet import *
+
+input_layer = 2
+hidden_1 = 4
+hidden_2 = 4
+hidden_3 = 4
+output_layer = 2
+
+nn = NeuralNetwork(input_layer, hidden_1, hidden_2, hidden_3, output_layer)
+
+def createAndData(i):
+	if(i == 0):
+		return [[0, 0], [0, 1]]
+	elif i == 1:
+		return [[1, 0], [0, 1]]
+	elif i == 2:
+		return [[0, 1], [0, 1]]
+	else:
+		return [[1, 1], [1, 0]]
+
+for j in range(0, 100):
+	for i in range(0, 4):
+		data = createAndData(i)
+		inputs = data[0]
+		target = data[1]
+		print inputs
+		print target
+		nn.train([inputs], [target], 10)
+
+for i in range(0, 4):
+	data = createAndData(i)
+	inputs = data[0]
+	print "inputs", inputs
+	pred = nn.predict([inputs])
+	for i in range(len(pred)):
+		pred[i] = round(pred[i])
+	print "predictions:",  pred
+
