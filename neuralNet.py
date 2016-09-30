f4edb92b25b214258c5c309d68bab80fca936ccb
Brandon Herbert <brandon@Brandons-MacBook-Pro.local>
2016-09-30 09:42:26 -0700

    update

--- neuralNet.py
+++ neuralNet.py
@@ -14,6 +14,8 @@ class NeuralNetwork:
 		self.y = tf.placeholder('float', [None, self.n_classes])
 
 		self.prediction = self.neural_network_model(self.x)
+		self.deep_dream = self.neural_network_model_dd(self.y)
+
 		if cost_func == 'reduce_sum':
 			self.model = 'softplus'
 			self.cost = 0.5 * tf.reduce_sum(tf.sub(self.prediction, self.y) * tf.sub(self.prediction, self.y))
@@ -59,14 +61,57 @@ class NeuralNetwork:
 		else:
 			l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
 			l1 = tf.nn.relu(l1)
-			l1 = tf.nn.softmax(l1)
+				# l1 = tf.nn.softmax(l1)
 			l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
 			l2 = tf.nn.relu(l2)
-			l2 = tf.nn.softmax(l2)
+				# l2 = tf.nn.softmax(l2)
 			l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
 			l3 = tf.nn.relu(l3)
-			l3 = tf.nn.softmax(l3)
-			output = tf.nn.softmax(tf.matmul(l3, output_layer['weights']) + output_layer['biases'])
+				# l3 = tf.nn.softmax(l3)
+			output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+				# output = tf.nn.softmax(tf.matmul(l3, output_layer['weights']) + output_layer['biases'])
+
+		return output
+
+	def neural_network_model_dd(self, data):
+
+		
+		hidden_1_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_classes, self.n_nodes_hl3])),
+			'biases': tf.Variable(tf.random_normal([self.n_nodes_hl3]))
+		}
+
+		hidden_2_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_nodes_hl3, self.n_nodes_hl2])),
+			'biases': tf.Variable(tf.random_normal([self.n_nodes_hl2]))
+		}
+
+		hidden_3_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_nodes_hl2, self.n_nodes_hl1])),
+			'biases': tf.Variable(tf.random_normal([self.n_nodes_hl1]))
+		}
+
+		output_layer = {
+			'weights': tf.Variable(tf.random_normal([self.n_nodes_hl1, self.input_node])),
+			'biases': tf.Variable(tf.random_normal([self.input_node]))
+		}
+
+		# l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
+		# l1 = tf.nn.relu(l1)
+				# l1 = tf.nn.softmax(l1)
+		# l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
+		# l2 = tf.nn.relu(l2)
+				# l2 = tf.nn.softmax(l2)
+		# l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
+		# l3 = tf.nn.relu(l3)
+				# l3 = tf.nn.softmax(l3)
+		# output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+
+		l1 = tf.nn.softplus(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
+		l2 = tf.nn.softplus(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
+		l3 = tf.nn.softplus(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
+		output = tf.nn.softplus(tf.matmul(l3, output_layer['weights']) + output_layer['biases'])
+
 		return output
 
 	def train(self, input_data, output_data, epochs):
@@ -82,3 +127,7 @@ class NeuralNetwork:
 	def predict(self, input_data):
 		p = self.sess.run(self.prediction, feed_dict={self.x: input_data})
 		return p[0]
+
+	def deepDream(self, input_data):
+		dd = self.sess.run(self.deep_dream, feed_dict={self.y: input_data})
+		return dd
--- playground.py
+++ playground.py
@@ -6,7 +6,7 @@ hidden_2 = 4
 hidden_3 = 4
 output_layer = 2
 
-nn = NeuralNetwork(input_layer, hidden_1, hidden_2, hidden_3, output_layer, 0.0005, 'adam', 'reduce_sum1')
+nn = NeuralNetwork(input_layer, hidden_1, hidden_2, hidden_3, output_layer, 0.0005, 'adam', 'reduce_sum')
 
 def createAndData(i):
 	if(i == 0):
